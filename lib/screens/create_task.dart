1bdb331a699d1b3f4afcece4c33199f0ee8d7540
Mathias Petersen <mathias.petersen95@gmail.com>
2019-11-25 01:12:06 +0100

    #31 add scrollview to create task screen

--- lib/screens/create_task.dart
+++ lib/screens/create_task.dart
@@ -21,78 +21,80 @@ class _CreateTaskScreenState extends State<CreateTaskScreen> {
       appBar: AppBar(
         title: Text("Create Task"),
       ),
-      body: Container(
-          padding: EdgeInsets.all(20.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: 20.0),
-                Text(
-                  'Create Task',
-                  style: TextStyle(fontSize: 20),
-                ),
-                SizedBox(height: 20.0),
-                TextFormField(
-                    keyboardType: TextInputType.text,
-                    decoration: InputDecoration(labelText: "Title")),
-                TextFormField(
-                  keyboardType: TextInputType.text,
-                  decoration: InputDecoration(labelText: "Description"),
-                  maxLines: null,
-                ),
-                TextFormField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(labelText: "Payment")),
-                DateTimeField(
-                  format: DateFormat("yyyy-MM-dd HH:mm"),
-                  decoration: InputDecoration(labelText: "Date & Time"),
-                  onShowPicker: (context, currentValue) async {
-                    final date = await showDatePicker(
-                        context: context,
-                        firstDate: DateTime(1900),
-                        initialDate: currentValue ?? DateTime.now(),
-                        lastDate: DateTime(2100));
-                    if (date != null) {
-                      final time = await showTimePicker(
-                        context: context,
-                        initialTime: TimeOfDay.fromDateTime(
-                            currentValue ?? DateTime.now()),
-                      );
-                      return DateTimeField.combine(date, time);
-                    } else {
-                      return currentValue;
-                    }
-                  },
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  alignment: Alignment.centerLeft,
-                  child: Column(
-                    children: <Widget>[
-                      Text("Hourly"),
-                      Checkbox(
-                        value: _hourly,
-                        onChanged: (bool value) {
-                          setState(() {
-                            _hourly = value;
-                          });
-                        },
-                      ),
-                    ],
+      body: SingleChildScrollView(
+        child: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 20.0),
+                  Text(
+                    'Create Task',
+                    style: TextStyle(fontSize: 20),
                   ),
-                ),
-                RaisedButton(
-                  child: Text("Create Task!"),
-                  onPressed: () async {
-                    final form = _formKey.currentState;
-                  },
-                ),
-              ],
-            ),
-          )),
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(labelText: "Title")),
+                  TextFormField(
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(labelText: "Description"),
+                    maxLines: null,
+                  ),
+                  TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(labelText: "Payment")),
+                  DateTimeField(
+                    format: DateFormat("yyyy-MM-dd HH:mm"),
+                    decoration: InputDecoration(labelText: "Date & Time"),
+                    onShowPicker: (context, currentValue) async {
+                      final date = await showDatePicker(
+                          context: context,
+                          firstDate: DateTime(1900),
+                          initialDate: currentValue ?? DateTime.now(),
+                          lastDate: DateTime(2100));
+                      if (date != null) {
+                        final time = await showTimePicker(
+                          context: context,
+                          initialTime: TimeOfDay.fromDateTime(
+                              currentValue ?? DateTime.now()),
+                        );
+                        return DateTimeField.combine(date, time);
+                      } else {
+                        return currentValue;
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    child: Column(
+                      children: <Widget>[
+                        Text("Hourly"),
+                        Checkbox(
+                          value: _hourly,
+                          onChanged: (bool value) {
+                            setState(() {
+                              _hourly = value;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  RaisedButton(
+                    child: Text("Create Task!"),
+                    onPressed: () async {
+                      final form = _formKey.currentState;
+                    },
+                  ),
+                ],
+              ),
+            )),
+      ),
     );
   }
 }
